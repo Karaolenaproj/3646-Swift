291fb055adf419065ea6951d08d5bfda7d9342b6
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-11-13 15:50:49 +0200

    init

---
+++ BeakOfDuty/App/BeakOfDutyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeakOfDutyApp.swift
+//  BeakOfDuty
+//
+//  Created by Serhii Babchuk on 13.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct BeakOfDutyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BDLaunchView()
+        }
+    }
+}
